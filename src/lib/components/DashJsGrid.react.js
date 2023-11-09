611fdde348f9a4a87ae857172682b24b95d6bbbe
TomaszRewak <tomaszrewak@gmail.com>
2023-11-10 00:45:29 +0800

    Removing template code.

--- src/lib/components/DashJsGrid.react.js
+++ src/lib/components/DashJsGrid.react.js
@@ -1,61 +1,22 @@
-import React, { useState } from 'react';
+/* eslint-disable import/prefer-default-export */
+import React from 'react';
 import PropTypes from 'prop-types';
 
-/**
- * ExampleComponent is an example component.
- * It takes a property, `label`, and
- * displays it.
- * It renders an input with the property `value`
- * which is editable by the user.
- */
-const DashJsGrid = (props) => {
-    const {id, label, setProps, value} = props;
-
+function DashJsGrid({ title }) {
     return (
-        <div id={id}>
-            ExampleComponent: {label}&nbsp;
-            <input
-                value={value}
-                onChange={
-                    /*
-                        * Send the new value to the parent component.
-                        * setProps is a prop that is automatically supplied
-                        * by dash's front-end ("dash-renderer").
-                        * In a Dash app, this will update the component's
-                        * props and send the data back to the Python Dash
-                        * app server if a callback uses the modified prop as
-                        * Input or State.
-                        */
-                    e => setProps({ value: e.target.value })
-                }
-            />
+        <div>
+            Test 2: {title}
         </div>
     );
-}
-
-DashJsGrid.defaultProps = {};
+};
 
 DashJsGrid.propTypes = {
-    /**
-     * The ID used to identify this component in Dash callbacks.
-     */
-    id: PropTypes.string,
+    // The header text of the component
+    title: PropTypes.string.isRequired,
+};
 
-    /**
-     * A label that will be printed when this component is rendered.
-     */
-    label: PropTypes.string.isRequired,
-
-    /**
-     * The value displayed in the input.
-     */
-    value: PropTypes.string,
-
-    /**
-     * Dash-assigned callback that should be called to report property changes
-     * to Dash, to make them available for callbacks.
-     */
-    setProps: PropTypes.func
+DashJsGrid.defaultProps = {
+    title: 'Hello World!'
 };
 
 export default DashJsGrid;
--- usage.py
+++ usage.py
@@ -5,17 +5,15 @@ app = Dash(__name__)
 
 app.layout = html.Div([
     dash_js_grid.DashJsGrid(
-        id='input',
-        value='my-value',
-        label='my-label'
+        title= 'My Grid',
     ),
     html.Div(id='output')
 ])
 
 
-@callback(Output('output', 'children'), Input('input', 'value'))
-def display_output(value):
-    return 'You have entered {}'.format(value)
+# @callback(Output('output', 'children'), Input('input', 'value'))
+# def display_output(value):
+#     return 'You have entered {}'.format(value)
 
 
 if __name__ == '__main__':
