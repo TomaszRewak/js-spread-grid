184d4c3b92ce40bde4839bf80159aadaa2edf07d
TomaszRewak <tomaszrewak@gmail.com>
2023-11-11 18:39:29 +0800

    Using the device pixel ratio.

--- src/lib/fragments/GridCanvas.react.js
+++ src/lib/fragments/GridCanvas.react.js
@@ -1,4 +1,5 @@
-import { useCallback, useEffect, useState } from "react"
+import React, { useCallback, useEffect, useState } from "react"
+import useDevicePixelRatio from "../hooks/useDevicePixelRatio";
 
 /**
  * @param {{
@@ -10,23 +11,36 @@ import { useCallback, useEffect, useState } from "react"
 export default function GridCanvas({
     cells
 }) {
-    const canvasRef = useState(null);
+    const [canvas, setCanvas] = useState(null);
+    const devicePixelRatio = useDevicePixelRatio();
 
-    const setCanvasRef = useCallback((node) => {
-        canvasRef.current = node;
+    const updateCanvas = useCallback((node) => {
+        setCanvas(node);
     }, []);
 
     useEffect(() => {
         const draw = () => {
-            const canvas = canvasRef.current;
+            if (!canvas)
+                return;
+
+            // Checking how often this is called
+            console.log('draw');
+
+            console.log(devicePixelRatio);
+
             const ctx = canvas.getContext("2d");
             const cellSize = 20;
             const width = cells[0].length * cellSize;
             const height = cells.length * cellSize;
 
-            canvas.width = width;
-            canvas.height = height;
+            canvas.width = width * devicePixelRatio;
+            canvas.height = height * devicePixelRatio;
+            canvas.style.width = `${width}px`;
+            canvas.style.height = `${height}px`;
 
+            ctx.scale(devicePixelRatio, devicePixelRatio);
+
+            // Draw cells
             cells.forEach((row, y) => {
                 row.forEach((cell, x) => {
                     ctx.fillStyle = "#fff";
@@ -35,14 +49,25 @@ export default function GridCanvas({
                     ctx.fillText(cell.value, x * cellSize + 10, y * cellSize + 10);
                 });
             });
+
+            // Draw borders
+            ctx.strokeStyle = "#000";
+            ctx.lineWidth = 1 / devicePixelRatio;
+            ctx.beginPath();
+            cells.forEach((row, y) => {
+                row.forEach((cell, x) => {
+                    ctx.rect(x * cellSize + 1, y * cellSize + 1, cellSize - 2, cellSize - 2);
+                });
+            });
+            ctx.stroke();
         };
 
         const nextFrame = requestAnimationFrame(draw);
 
         return () => cancelAnimationFrame(nextFrame);
-    }, [cells, canvasRef]);
+    }, [cells, canvas, devicePixelRatio]);
 
     return (
-        <canvas ref={setCanvasRef} />
+        <canvas ref={updateCanvas} />
     )
 }
---
+++ src/lib/hooks/useDevicePixelRatio.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+// TODO: Test this hook
+function useDevicePixelRatio() {
+    const [devicePixelRatio, setDevicePixelRatio] = useState(window.devicePixelRatio);
+
+    useEffect(() => {
+        const media = matchMedia(`(resolution: ${devicePixelRatio}dppx)`);
+        const updateDevicePixelRatio = () => {
+            console.log('Device pixel ratio changed')
+            setDevicePixelRatio(window.devicePixelRatio);
+        };
+
+        media.addEventListener('change', updateDevicePixelRatio, { once: true });
+
+        return () => media.removeEventListener('change', updateDevicePixelRatio);
+    }, [devicePixelRatio]);
+
+    return devicePixelRatio;
+}
+
+export default useDevicePixelRatio;
