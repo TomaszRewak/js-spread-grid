888c1f1b9f2170938247b51d8cebc52741250ae8
TomaszRewak <tomaszrewak@gmail.com>
2023-12-14 17:56:51 +0100

    Adding example page.

---
+++ example/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SpreadGrid from '../../src/components/SpreadGrid';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SpreadGrid />
+  </React.StrictMode>
+);
