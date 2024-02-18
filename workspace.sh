93c089cad9f8e1f5877a585f03d9c5a94f7a1f7a
TomaszRewak <tomaszrewak@gmail.com>
2024-02-18 13:01:09 +0100

    Add docs to the workspace.

--- workspace.sh
+++ workspace.sh
@@ -1,5 +1,7 @@
 workspace="spread-grid"
 
+cd "$(dirname "$0")"
+
 code ./.vscode/spread-grid.code-workspace
 
 tmux new-session -d -s "$workspace"
@@ -12,8 +14,13 @@ tmux send-keys -t "$workspace:2" "tmux kill-session -t $workspace"
 
 tmux new-window -t "$workspace:3" -n "example"
 tmux split-window -h -t "$workspace:3"
-tmux send-keys -t "$workspace:3.0" "cd example" C-m "npm start" C-m
+tmux send-keys -t "$workspace:3.0" "cd example" C-m "PORT=3000 npm start" C-m
 tmux send-keys -t "$workspace:3.1" "cd example" C-m "npm run watch-src" C-m
+
+tmux new-window -t "$workspace:4" -n "docs"
+tmux split-window -h -t "$workspace:4"
+tmux send-keys -t "$workspace:4.0" "cd docs" C-m "PORT=3001 npm start" C-m
+tmux send-keys -t "$workspace:4.1" "cd docs" C-m "npm run watch-src" C-m
 
 tmux select-window -t "$workspace:3"
 tmux attach-session -t "$workspace"
