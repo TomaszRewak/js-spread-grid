39ab649f4c7699bdf6494fd014f143511d51e47c
TomaszRewak <tomaszrewak@gmail.com>
2024-02-19 01:13:15 +0100

    Working on docs.

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import SpreadGrid from './spread-grid/components/SpreadGrid';
+import CodeBlock from './CodeBlock';
+import { Drawer, List, ListItem, ListItemButton } from '@mui/material';
 
 function App() {
   const example = <SpreadGrid
@@ -10,24 +12,50 @@ function App() {
     ]}
   />;
   const exampleCode = [
-    '<SpreadGrid',
-    '  data={[',
-    '    { name: \'John\', age: 25, score: 100 },',
-    '    { name: \'Jane\', age: 24, score: 90 },',
-    '    { name: \'Jack\', age: 26, score: 80 },',
-    '  ]}',
-    '/>'
-  ].join('\n');
+    {
+      framework: 'React',
+      grammar: 'javascript',
+      language: 'jsx',
+      code: [
+        '<SpreadGrid',
+        '  data={[',
+        '    { name: \'John\', age: 25, score: 100 },',
+        '    { name: \'Jane\', age: 24, score: 90 },',
+        '    { name: \'Jack\', age: 26, score: 80 },',
+        '  ]}',
+        '/>'
+      ].join('\n')
+    },
+    {
+      framework: 'js',
+      grammar: 'javascript',
+      language: 'javascript',
+      code: [
+        'createGrid(div, {',
+        '  data: [',
+        '    { name: \'John\', age: 25, score: 100 },',
+        '    { name: \'Jane\', age: 24, score: 90 },',
+        '    { name: \'Jack\', age: 26, score: 80 },',
+        '  ]',
+        '});'
+      ].join('\n')
+    }
+  ];
 
   return (
-    <div className="App">
-      <code className="language-js">
-        <pre>
-          {exampleCode}
-        </pre>
-      </code>
-      {example}
-    </div>
+    <>
+      <Drawer variant="permanent">
+        <List>
+          <ListItemButton>Item 1</ListItemButton>
+          <ListItemButton>Item 2</ListItemButton>
+          <ListItemButton>Item 3</ListItemButton>
+        </List>
+      </Drawer>
+      <main className="App">
+        <CodeBlock language="javascript" options={exampleCode} />
+        {example}
+      </main>
+    </>
   );
 }
 
