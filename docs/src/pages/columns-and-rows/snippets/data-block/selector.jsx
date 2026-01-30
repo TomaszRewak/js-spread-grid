5f2168e4161b36dfa3551e4d5ac6ad36c2877299
Tomasz Rewak <tomaszrewak@gmail.com>
2026-01-30 20:29:16 +0800

    Fixing selectors

--- docs/src/pages/columns-and-rows/snippets/data-block/selector.jsx
+++ docs/src/pages/columns-and-rows/snippets/data-block/selector.jsx
@@ -7,13 +7,13 @@
     columns={[
         {
             type: 'DATA-BLOCK',
-            selector: data => Array.from({ length: data.columns }, (_, index) => index + 1)
+            selector: ({ data }) => Array.from({ length: data.columns }, (_, index) => index + 1)
         },
     ]}
     rows={[
         {
             type: 'DATA-BLOCK',
-            selector: data => Array.from({ length: data.rows }, (_, index) => index + 1)
+            selector: ({ data }) => Array.from({ length: data.rows }, (_, index) => index + 1)
         },
     ]}
     dataSelector={({ data, column, row }) => column.selector * row.selector + data.offset}
